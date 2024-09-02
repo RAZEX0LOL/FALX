7c198d6e05c843ae25e0046de40a8a7eee9e9772
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2024-09-02 15:09:24 +0400

    update text

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Начальный текст
-    updateDescription("Пожалуйста, наведите курсор на элементы справа, чтобы увидеть детали.");
+    // updateDescription("Пожалуйста, наведите курсор на элементы справа, чтобы увидеть детали.");
 
     items.forEach(item => {
         item.addEventListener("mouseenter", function() {
